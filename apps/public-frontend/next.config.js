1a93ad3c9afb7ba1a0bbe2e35b3ee1969aa68e64
TimurBas <timurbas1@hotmail.com>
2023-05-22 01:17:43 +0200

    next configs

--- apps/public-frontend/next.config.js
+++ apps/public-frontend/next.config.js
@@ -1,8 +1,4 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    appDir: true
-  },
+module.exports = {
   transpilePackages: ["ui"],
   reactStrictMode: false,
   async headers() {
@@ -21,4 +17,3 @@ const nextConfig = {
   }
 }
 
-module.exports = nextConfig
