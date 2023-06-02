0825873e4ea29bc30b8327d20eee582d63597fa5
TimurBas <timurbas1@hotmail.com>
2023-06-02 22:32:50 +0200

    test

--- apps/public-frontend/src/app/page.tsx
+++ apps/public-frontend/src/app/page.tsx
@@ -7,13 +7,13 @@ export default function Home() {
   const randomIndex = Math.floor(Math.random() * movieTitles.length);
   const movieTitleObj = movieTitles[randomIndex];
   const title = movieTitleObj.title;
-
+  const test = "";
   return (
     <>
       <div className="flex-col justify-between">
         <HeroSection />
       </div>
-      <div className="flex flex-col gap-y-20 items-center justify-center pt-24">
+      <div className="flex flex-col items-center justify-center gap-y-20 pt-24">
         <ChatGptGeneratedText title={title} />
         <StabilityGeneratedImage title={title} />
       </div>
