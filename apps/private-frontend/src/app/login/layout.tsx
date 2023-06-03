bb7392d442299ad201e0b15420284d771b538d87
TimurBas <timurbas1@hotmail.com>
2023-06-03 18:27:35 +0200

    fix

--- apps/private-frontend/src/app/login/layout.tsx
+++ apps/private-frontend/src/app/login/layout.tsx
@@ -1,9 +1,6 @@
 "use client";
 import React from "react";
-import useIsAuthenticated from "../../supabase/useIsAuthenticated";
 
 export default function layout({ children }: { children: React.ReactNode }) {
-  const isAuthenticated = useIsAuthenticated();
-
   return <div>{children}</div>;
 }
---
+++ apps/private-frontend/src/supabase/AuthProtected.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AuthProtected = () => {
+  return <div>AuthProtected</div>;
+};
+
+export default AuthProtected;
