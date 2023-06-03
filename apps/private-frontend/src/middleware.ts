293b5e5376ce5d2c1a7c13dd36edfbec39241922
TimurBas <timurbas1@hotmail.com>
2023-06-04 01:34:09 +0200

    wip

--- apps/private-frontend/src/middleware.ts
+++ apps/private-frontend/src/middleware.ts
@@ -4,6 +4,17 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 import { Database } from "supa";
 
+const resolveRedirectUrl = () => {
+  const env = process.env.NEXT_PUBLIC_VERCEL_ENV;
+  if (env === "production") return process.env.PROD_PUBLIC_FRONTEND_URL;
+  if (env === "preview")
+    `https://public-frontend-${process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_REF.replaceAll(
+      "/",
+      "-"
+    )}-timurbas.vercel.app`;
+  return process.env.LOCAL_PUBLIC_FRONTEND_URL;
+};
+
 export async function middleware(req: NextRequest) {
   const res = NextResponse.next();
   const supabase = createMiddlewareClient<Database>({ req, res });
@@ -12,6 +23,6 @@ export async function middleware(req: NextRequest) {
   } = await supabase.auth.getSession();
 
   if (!session)
-    return NextResponse.redirect(new URL("/login", "http://localhost:3000"));
+    return NextResponse.redirect(new URL("/login", resolveRedirectUrl()));
   return res;
 }
