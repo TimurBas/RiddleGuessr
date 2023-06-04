d80587c3efb065dac584b081a48bbe93dcbaeb75
Timur Bas <1603-au632749@users.noreply.gitlab.au.dk>
2023-06-04 14:05:10 +0200

    haydeee

--- apps/public-frontend/src/app/api/auth/route.ts
+++ apps/public-frontend/src/app/api/auth/route.ts
@@ -1,8 +1,22 @@
 import { NextResponse } from "next/server";
 
 export async function GET(request: Request) {
+  const resolveRedirectUrl = () => {
+    const env = process.env.NEXT_PUBLIC_VERCEL_ENV;
+
+    if (env === "production")
+      return `https://${process.env.PROD_PRIVATE_FRONTEND_URL}`;
+    if (env === "preview")
+      return `https://private-frontend-git-${process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_REF.replaceAll(
+        "/",
+        "-"
+      )}-timurbas.vercel.app`;
+
+    return process.env.LOCAL_PRIVATE_FRONTEND_URL;
+  };
+
   try {
-    return NextResponse.redirect("https://app.riddleguessr.com");
+    return NextResponse.redirect(new URL("/", resolveRedirectUrl()));
   } catch (error: any) {
     if (error.response) {
       console.log(error.response.status);
