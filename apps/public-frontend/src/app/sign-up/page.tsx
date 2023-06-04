fd981df9fddeb69766307e4b8bc74b8d7e331277
TimurBas <timurbas1@hotmail.com>
2023-06-04 17:38:20 +0200

    sign out impl

---
+++ apps/public-frontend/src/app/sign-up/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import SignUpSection from "../../components/features/signup/SignUpSection";
+
+const SignUp = () => {
+  return <SignUpSection />;
+};
+export default SignUp;
---
+++ apps/public-frontend/src/components/features/signup/SignUpSection.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import SignUpForm from "../../ui-smart/SignUpForm";
+
+const SignUpSection = () => {
+  return (
+    <div>
+      <SignUpForm />
+    </div>
+  );
+};
+
+export default SignUpSection;
