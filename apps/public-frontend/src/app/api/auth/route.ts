ca48143badc49f1f06085004659b9f462919969a
TimurBas <timurbas1@hotmail.com>
2023-06-04 16:39:32 +0200

    leggo

--- apps/public-frontend/src/app/api/auth/route.ts
+++ apps/public-frontend/src/app/api/auth/route.ts
@@ -4,10 +4,8 @@ export async function GET(request: Request) {
   const resolveRedirectUrl = () => {
     const env = process.env.NEXT_PUBLIC_VERCEL_ENV;
 
-    if (env === "production")
-      return `https://${process.env.PROD_PRIVATE_FRONTEND_URL}`;
-    if (env === "preview")
-      return `https://${process.env.STAGING_PRIVATE_FRONTEND_URL}`;
+    if (env === "production") return `${process.env.PROD_PRIVATE_FRONTEND_URL}`;
+    if (env === "preview") return `${process.env.STAGING_PRIVATE_FRONTEND_URL}`;
     return process.env.LOCAL_PRIVATE_FRONTEND_URL;
   };
 
