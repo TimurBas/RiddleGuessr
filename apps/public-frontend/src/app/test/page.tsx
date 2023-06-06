39692474452112814103097f8e501b2477716fcf
Timur Bas <timurbas1@hotmail.com>
2023-06-07 01:18:40 +0200

    hajdeee

--- apps/public-frontend/src/app/test/page.tsx
+++ apps/public-frontend/src/app/test/page.tsx
@@ -2,10 +2,18 @@ import React from "react";
 import { cookies } from "next/headers";
 import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 import { Database } from "supa";
+import Base64Image from "../../components/ui/atomic/Base64Image";
 
 export default async function ServerComponent() {
   const supabase = createServerComponentClient<Database>({ cookies });
   const { data } = await supabase.from("images").select();
-  console.log(data);
-  return <div>Test</div>;
+  const first = data[0];
+  return (
+    <div>
+      <Base64Image base64Image={first.image_base64} />
+      {first.movie_id}
+      {first.created_at}
+      {first.id}
+    </div>
+  );
 }
