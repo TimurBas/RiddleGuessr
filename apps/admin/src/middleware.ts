7621bf2b502d6ba39830fe71f58f8efc2f8fd43f
TimurBas <timurbas1@hotmail.com>
2023-06-11 01:36:04 +0200

    oops

--- apps/admin/src/middleware.ts
+++ apps/admin/src/middleware.ts
@@ -26,6 +26,7 @@ export const config = {
   matcher: [
     /*
      * Match all request paths except for the ones starting with:
+     * - api (API routes)
      * - _next/static (static files)
      * - _next/image (image optimization files)
      * - favicon.ico (favicon file)
