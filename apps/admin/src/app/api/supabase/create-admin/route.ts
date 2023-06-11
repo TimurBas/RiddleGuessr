832269359fb2df0dd628dee02d0a960340099fd3
TimurBas <timurbas1@hotmail.com>
2023-06-11 22:29:42 +0200

    .

--- apps/admin/src/app/api/supabase/create-admin/route.ts
+++ apps/admin/src/app/api/supabase/create-admin/route.ts
@@ -2,10 +2,20 @@ import { NextResponse } from "next/server";
 import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
 import { Database } from "supa";
+import { createClient } from "@supabase/supabase-js";
 
 export async function GET(request: Request) {
   try {
-    const supabase = createRouteHandlerClient<Database>({ cookies });
+    const supabase = createClient(
+      process.env.NEXT_PUBLIC_SUPABASE_URL,
+      process.env.NEXT_PUBLIC_SUPABASE_SERVICE_ROLE,
+      {
+        auth: {
+          autoRefreshToken: false,
+          persistSession: false,
+        },
+      }
+    );
     const res = await supabase.auth.admin.createUser({
       email: "timurbas1@hotmail.com",
       password: "kolpol123987K",
