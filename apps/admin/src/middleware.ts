e030ee5378dfdfa85799e8f4c96b953f9cc2bbd4
TimurBas <timurbas1@hotmail.com>
2023-06-11 03:26:55 +0200

    env

--- apps/admin/src/middleware.ts
+++ apps/admin/src/middleware.ts
@@ -7,8 +7,8 @@ import { Database } from "supa";
 const resolveRedirectUrl = () => {
   const env = process.env.NEXT_PUBLIC_VERCEL_ENV;
 
-  if (env === "production") return process.env.PROD_PRIVATE_FRONTEND_URL;
-  if (env === "preview") return process.env.STAGING_PRIVATE_FRONTEND_URL;
+  if (env === "production") return process.env.PROD_ADMIN_URL;
+  if (env === "preview") return process.env.STAGING_ADMIN_URL;
   return process.env.LOCAL_PRIVATE_FRONTEND_URL;
 };
 
