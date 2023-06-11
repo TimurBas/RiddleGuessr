8de1afe5b2f0d00f95fddb301059067e259786ff
TimurBas <timurbas1@hotmail.com>
2023-06-11 04:32:23 +0200

    CORS

--- apps/public-frontend/src/middleware.ts
+++ apps/public-frontend/src/middleware.ts
@@ -13,13 +13,12 @@ const resolveRedirectUrl = () => {
 };
 
 export async function middleware(req: NextRequest) {
-  console.log(req.url);
   const res = NextResponse.next();
+  if (req.method === "OPTIONS") return res;
   const supabase = createMiddlewareClient<Database>({ req, res });
   const {
     data: { session },
   } = await supabase.auth.getSession();
-  console.log(session);
 
   const baseUrl = resolveRedirectUrl();
   if (session) return NextResponse.redirect(new URL("/", baseUrl));
