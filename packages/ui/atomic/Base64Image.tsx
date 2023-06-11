49679c3a97db6f6728be1b5f53905a2e99630e91
TimurBas <timurbas1@hotmail.com>
2023-06-12 01:34:06 +0200

    added base64image again

---
+++ packages/ui/atomic/Base64Image.tsx
@@ -0,0 +1,19 @@
+import Image from "next/image";
+import { FC } from "react";
+
+type Props = {
+  base64Image: string;
+};
+
+const Base64Image: FC<Props> = ({ base64Image }: Props) => {
+  return (
+    <Image
+      src={`data:image/png;base64,${base64Image}`}
+      alt="AI Generated Image"
+      width={192}
+      height={192}
+    />
+  );
+};
+
+export default Base64Image;
--- packages/ui/index.tsx
+++ packages/ui/index.tsx
@@ -10,6 +10,7 @@ export * from "./atomic/Header";
 export * from "./atomic/TopBar";
 export * from "./atomic/Emoji";
 export * from "./atomic/MovieImage";
+export * from "./atomic/Base64Image";
 export * from "./header/MainHeader";
 export * from "./auth/login/LoginForm";
 export * from "./auth/sign-up/SignUpForm";
