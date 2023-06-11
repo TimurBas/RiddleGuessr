e7cb04cb6665ed1459d60124ba1aa145a4ea07d1
TimurBas <timurbas1@hotmail.com>
2023-06-11 17:23:13 +0200

    CORS

--- apps/public-frontend/next.config.js
+++ apps/public-frontend/next.config.js
@@ -1,4 +1,28 @@
 module.exports = {
   transpilePackages: ["ui"],
   reactStrictMode: false,
+  async headers() {
+    return [
+      {
+        source: "/login",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          {
+            key: "Access-Control-Allow-Origin",
+            value:
+              "https://app.riddleguessr.com, https://staging-app.riddleguessr.com",
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, next-router-state-tree, next-url, rsc",
+          },
+        ],
+      },
+    ];
+  },
 };
