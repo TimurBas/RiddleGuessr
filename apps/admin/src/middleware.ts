d94fafc3b2e4b8922a082b54583db4b8d8931839
TimurBas <timurbas1@hotmail.com>
2023-06-11 22:38:33 +0200

    .

--- apps/admin/src/middleware.ts
+++ apps/admin/src/middleware.ts
@@ -20,9 +20,11 @@ export async function middleware(req: NextRequest) {
     data: { session },
   } = await supabase.auth.getSession();
 
+  const redirectRes = NextResponse.redirect("https://riddleguessr.com");
   const isLoginPage = req.nextUrl.pathname === "/login";
-  if (!session && !isLoginPage)
-    return NextResponse.redirect("https://riddleguessr.com");
+
+  if (!session && !isLoginPage) return redirectRes;
+  if (session.user.email !== "timurbas1@hotmail.com") return redirectRes;
 
   if (session && isLoginPage)
     return NextResponse.redirect(new URL("/platform", resolveRedirectUrl()));
